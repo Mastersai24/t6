dffd3ac4b1a0c42c77816a4f2b18d63846a0dfd2
Mastersai24 <113352988+Mastersai24@users.noreply.github.com>
2022-09-13 22:07:47 +0530

    Update day1sample.js

--- day1sample.js
+++ day1sample.js
@@ -2,9 +2,12 @@ const http = require('http');
 const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hi this is Praveen</h1>');
+    const number = parseInt(prompt("Enter an integer: "));
+for (let i = 1; i <= 10; i++) {
+  // multiply i with number
+  const result = i * number;
+  console.log(`${number} * ${i} = ${result}`);
+}
 });
 
 server.listen(port,() => {
