8912d4ffd3a8ed471b194a370248f4d9fc6491e7
Mastersai24 <113352988+Mastersai24@users.noreply.github.com>
2022-09-13 22:20:21 +0530

    Update day1sample.js

--- day1sample.js
+++ day1sample.js
@@ -2,12 +2,9 @@ const http = require('http');
 const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
-    const number = parseInt(prompt("Enter an integer: "));
 for (let i = 1; i <= 10; i++) {
-  // multiply i with number
-  const result = i * number;
-  console.log(`${number} * ${i} = ${result}`);
-}
+  const result = i * 10;
+  console.log(`10 * ${i} = ${result}`);}
 });
 
 server.listen(port,() => {
