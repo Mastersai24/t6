c3ae7aa77e5980a9250bb9b8e4bd3139f961c70d
Mastersai24 <113352988+Mastersai24@users.noreply.github.com>
2022-09-13 21:23:06 +0530

    Update day1sample.js

--- day1sample.js
+++ day1sample.js
@@ -1,32 +1,13 @@
-// program to check if a number is prime or not
+// program to generate a multiplication table
 
 // take input from the user
-const number = parseInt(prompt("Enter a positive number: "));
-let isPrime = true;
+const number = parseInt(prompt("Enter an integer: "));
 
-// check if number is equal to 1
-if (number === 1) {
-  console.log("1 is neither prime nor composite number.");
-}
+//creating a multiplication table
+for (let i = 1; i <= 10; i++) {
+  // multiply i with number
+  const result = i * number;
 
-// check if number is greater than 1
-else if (number > 1) {
-  // looping through 2 to number-1
-  for (let i = 2; i < number; i++) {
-    if (number % i == 0) {
-      isPrime = false;
-      break;
-    }
-  }
-
-  if (isPrime) {
-    console.log(`${number} is a prime number`);
-  } else {
-    console.log(`${number} is a not prime number`);
-  }
-}
-
-// check if number is less than 1
-else {
-  console.log("The number is not a prime number.");
+  // display the result
+  console.log(`${number} * ${i} = ${result}`);
 }
